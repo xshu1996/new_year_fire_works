5c6c2ad0e6a3a5267b7a33ecd76da6eeb48af0f3
xshu <825947170@qq.com>
2021-12-30 20:11:49 +0800

    init

---
+++ src/settings.fb45f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","mid"],collisionMatrix:[[false],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"6c0a3",main:"1436c"}};
